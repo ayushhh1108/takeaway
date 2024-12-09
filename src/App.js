3f8194f0375727cd3650098e0a7c36b706968098
ayush@4718 <ayushhirapara1108@gmail.com>
2024-12-09 18:49:19 +0530

    remove unless code files

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
-import RegisterPage from './Pages/RegistrationPage';
 import LoginPage from './Pages/LoginPage';
 import { createTheme, ThemeProvider } from "@mui/material";
-import HomePage from "./Pages/LandingPage";
 import MainPage from "./Pages/MainPage";
 import 'react-toastify/dist/ReactToastify.css';
-import PrivacyPolicy from './Pages/AboutUsPage';
 import TermsConditions from './Pages/TermsConditionsPage';
-import UserDashboard from './components/UserDashboard/UserDashboard';
-import ProfilePage from './Pages/Profile';
-import ReferralPage from './Pages/ReferralPage/Referral';
-import Withdraw from './Pages/WithdrawPage';
-import Surveys from './Pages/Surveys';
 import { useDispatch } from "react-redux";
 import { getUserDetails } from './Pages/action';
 
@@ -42,18 +34,9 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/" element={<MainPage />} >
-              <Route index element={<HomePage />} />
-              <Route path='/:id' element={<HomePage />} />
-              <Route path='/Privacy-Policy' element={<PrivacyPolicy />} />
               <Route path='/Terms-Conditions' element={<TermsConditions />} />
             </Route>
-            <Route path="/register" exact element={<RegisterPage />} />
             <Route path="/login" exact element={<LoginPage />} />
-            <Route path="/dashboard" exact element={<UserDashboard />} />
-            <Route path="/withdraw" exact element={<Withdraw />} />
-            <Route path="/referrals" exact element={<ReferralPage />} />
-            <Route path="/surveys" exact element={<Surveys />} />
-            <Route path="/profile" exact element={<ProfilePage />} />
           </Routes>
         </div>
       </ThemeProvider>
