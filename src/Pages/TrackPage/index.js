6a4008ffd808ad58277228587c8053cd45d762ff
ayush@4718 <ayushhirapara1108@gmail.com>
2024-12-28 15:01:10 +0530

    socket and track order completed

--- src/Pages/TrackPage/index.js
+++ src/Pages/TrackPage/index.js
@@ -8,17 +8,41 @@ import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getOrderData } from "../action";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { io } from "socket.io-client";
 
 const OrderStatus = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const [orderStatus, setStatus] = useState("");
+  const [updatedStatus, setUpdateStatus] = useState();
+
+  const socket = io(process.env.REACT_APP_API_BASE_URL); // Replace with your server URL
+
+  useEffect(() => {
+    socket.emit("joinOrder", params?.id);
+    socket.on("orderUpdated", (updatedOrder) => {
+      console.log("Order updated:", updatedOrder);
+      setStatus(updatedOrder?.status);
+      setUpdateStatus(updatedOrder);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  console.log("orderStatus", orderStatus);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const orderData = await dispatch(getOrderData(params?.id));
         setStatus(orderData?.status);
+        setUpdateStatus({
+          status: orderData?.status,
+          reason: orderData?.reason,
+        });
       } catch (error) {
         console.error("Error fetching order data:", error);
       }
@@ -58,7 +82,7 @@ const OrderStatus = () => {
         className="main-tracktitle"
         sx={{ textAlign: "center" }}
       >
-        Your Brew is on the Move!
+        Your Brew is {orderStatus === "Rejected" ? "not" : "on"} the Move!
       </Typography>
       <Typography
         variant="body2"
@@ -73,131 +97,185 @@ const OrderStatus = () => {
       </Typography>
 
       {/* Status Steps */}
-      <Grid container spacing={2} sx={{ maxWidth: 400 }}>
-        {/* Step 1 */}
-        <Grid
-          item
-          xs={12}
-          className={orderStatus === "Preparing" ? "" : "incomplete-step"}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              backgroundColor: "#e8f5e9", // Light green background
-              borderRadius: "8px",
-              padding: "8px",
-            }}
+      {orderStatus !== "Rejected" ? (
+        <Grid container spacing={2} sx={{ maxWidth: 400 }}>
+          {/* Step 1 */}
+          <Grid
+            item
+            xs={12}
+            className={
+              orderStatus === "Preparing" || orderStatus === "Completed"
+                ? ""
+                : "incomplete-step"
+            }
           >
-            <Avatar
+            <Box
               sx={{
-                backgroundColor: "#4caf50", // Green background
-                marginRight: "12px",
+                display: "flex",
+                alignItems: "center",
+                backgroundColor: "#e8f5e9", // Light green background
+                borderRadius: "8px",
+                padding: "8px",
               }}
             >
-              <CheckCircleIcon sx={{ color: "white" }} />
-            </Avatar>
-            <Box className="text-align-left">
-              <Typography
-                className="track-title"
-                variant="body1"
-                sx={{ fontWeight: "bold" }}
+              <Avatar
+                sx={{
+                  backgroundColor: "#4caf50", // Green background
+                  marginRight: "12px",
+                }}
               >
-                Order accepted
-              </Typography>
-              <Typography
-                className="track-decription"
-                variant="body2"
-                color="textSecondary"
-              >
-                Sit tight! We’ve got your order
-              </Typography>
+                <CheckCircleIcon sx={{ color: "white" }} />
+              </Avatar>
+              <Box className="text-align-left">
+                <Typography
+                  className="track-title"
+                  variant="body1"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Order accepted
+                </Typography>
+                <Typography
+                  className="track-decription"
+                  variant="body2"
+                  color="textSecondary"
+                >
+                  Sit tight! We’ve got your order
+                </Typography>
+              </Box>
             </Box>
-          </Box>
-        </Grid>
+          </Grid>
 
-        {/* Step 2 */}
-        <Grid
-          item
-          xs={12}
-          className={orderStatus === "Preparing" ? "" : "incomplete-step"}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              backgroundColor: "#fff8e1", // Light yellow background
-              borderRadius: "8px",
-              padding: "8px",
-            }}
+          {/* Step 2 */}
+          <Grid
+            item
+            xs={12}
+            className={
+              orderStatus === "Preparing" || orderStatus === "Completed"
+                ? ""
+                : "incomplete-step"
+            }
           >
-            <Avatar
+            <Box
               sx={{
-                backgroundColor: "#ffb74d", // Orange background
-                marginRight: "12px",
+                display: "flex",
+                alignItems: "center",
+                backgroundColor: "#fff8e1", // Light yellow background
+                borderRadius: "8px",
+                padding: "8px",
               }}
             >
-              <DeliveryDiningIcon sx={{ color: "white" }} />
-            </Avatar>
-            <Box className="text-align-left">
-              <Typography
-                variant="body1"
-                className="track-title"
-                sx={{ fontWeight: "bold" }}
+              <Avatar
+                sx={{
+                  backgroundColor: "#ffb74d", // Orange background
+                  marginRight: "12px",
+                }}
               >
-                In Preparing
-              </Typography>
-              <Typography
-                className="track-decription"
-                variant="body2"
-                color="textSecondary"
-              >
-                Our baristas are working their magic to perfect your order.
-                It’ll be ready for pickup shortly.
-              </Typography>
+                <DeliveryDiningIcon sx={{ color: "white" }} />
+              </Avatar>
+              <Box className="text-align-left">
+                <Typography
+                  variant="body1"
+                  className="track-title"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  In Preparing
+                </Typography>
+                <Typography
+                  className="track-decription"
+                  variant="body2"
+                  color="textSecondary"
+                >
+                  Our baristas are working their magic to perfect your order.
+                  It’ll be ready for pickup shortly.
+                </Typography>
+              </Box>
             </Box>
-          </Box>
-        </Grid>
+          </Grid>
 
-        {/* Step 3 */}
-        <Grid item xs={12} className={orderStatus === "Completed" ? "" : "incomplete-step"}>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              backgroundColor: "#e3f2fd", // Light blue background
-              borderRadius: "8px",
-              padding: "8px",
-            }}
+          {/* Step 3 */}
+          <Grid
+            item
+            xs={12}
+            className={orderStatus === "Completed" ? "" : "incomplete-step"}
           >
-            <Avatar
+            <Box
               sx={{
-                backgroundColor: "#90caf9", // Blue background
-                marginRight: "12px",
+                display: "flex",
+                alignItems: "center",
+                backgroundColor: "#e3f2fd", // Light blue background
+                borderRadius: "8px",
+                padding: "8px",
               }}
             >
-              <LocalShippingIcon sx={{ color: "white" }} />
-            </Avatar>
-            <Box className="text-align-left">
-              <Typography
-                variant="body1"
-                className="track-title"
-                sx={{ fontWeight: "bold" }}
+              <Avatar
+                sx={{
+                  backgroundColor: "#90caf9", // Blue background
+                  marginRight: "12px",
+                }}
               >
-                Pick Up Your Perfect Brew!
-              </Typography>
-              <Typography
-                className="track-decription"
-                variant="body2"
-                color="textSecondary"
-              >
-                Your order is ready and waiting! Swing by the shop to collect
-                your coffee and enjoy.
-              </Typography>
+                <LocalShippingIcon sx={{ color: "white" }} />
+              </Avatar>
+              <Box className="text-align-left">
+                <Typography
+                  variant="body1"
+                  className="track-title"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Pick Up Your Perfect Brew!
+                </Typography>
+                <Typography
+                  className="track-decription"
+                  variant="body2"
+                  color="textSecondary"
+                >
+                  Your order is ready and waiting! Swing by the shop to collect
+                  your coffee and enjoy.
+                </Typography>
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      ) : (
+        <Grid container spacing={2} sx={{ maxWidth: 400 }}>
+          {/* Step 1 */}
+          <Grid item xs={12} className={""}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                backgroundColor: "#cf39173b", // Light green background
+                borderRadius: "8px",
+                padding: "8px",
+              }}
+            >
+              <Avatar
+                sx={{
+                  backgroundColor: "red", // Green background
+                  marginRight: "12px",
+                }}
+              >
+                <CancelIcon sx={{ color: "white" }} />
+              </Avatar>
+              <Box className="text-align-left">
+                <Typography
+                  className="track-title"
+                  variant="body1"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Order Rejected
+                </Typography>
+                <Typography
+                  className="track-decription"
+                  variant="body2"
+                  color="textSecondary"
+                >
+                  {updatedStatus?.reason}
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      )}
     </Box>
   );
 };
